7a93e22cad1aa3e5d80063a28f96d6e69de3955a
Sergio Carro Albarrán <sergioca22@gmail.com>
2015-05-05 20:24:08 +0200

    terminado

--- passwd.py
+++ passwd.py
@@ -4,16 +4,21 @@ los usuarios y su shell, tomados del archivo /etc/passwd
 """
 #!/usr/bin/python
 
-
 fichero = open('/etc/passwd','r')
-lectura = fichero.readlines();
+lectura = fichero.readlines()
 fichero.close()
-diccionario = {}                        ## Diccionario vacio
 
-for linea in lectura
-    shell = linea.split(":")[-1]
-    usuario = linea.split(":")[0]
-    dicc[usuario] = shell
-    print "Usuario: " + usuario + ", Shell: " + dicc[usuario]
+diccionario = {}
 
-print "Hay " + str(len(dicc)) + " usuarios"
+for linea in lectura:
+	shell = linea.split(":")[-1]
+	usuario = linea.split(":")[0]
+	diccionario[usuario] = shell
+
+print "root: " + diccionario['root']
+
+try:
+	print "imaginario: " + diccionario['imaginario']
+except:
+	print "No existe ese usuario."
+
